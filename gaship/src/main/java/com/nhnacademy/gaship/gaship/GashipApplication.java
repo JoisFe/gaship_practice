9c73fe75adfc730e7e289e88cb73fa4f5a89b09b
조재철 <rhsnqk@gmail.com>
2022-07-05 17:13:11 +0900

    github action practice

---
+++ gaship/src/main/java/com/nhnacademy/gaship/gaship/GashipApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.gaship.gaship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GashipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GashipApplication.class, args);
+	}
+
+}
---
+++ gaship/src/test/java/com/nhnacademy/gaship/gaship/GashipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.gaship.gaship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GashipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
